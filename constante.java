929eaf5d4131115738f3a2eee76f6a3fdc672902
Rogiel <rogiel.anjos@gmail.com>
2021-08-31 13:29:50 -0300

    adicionando constante JAVA

---
+++ constante.java
@@ -0,0 +1,10 @@
+// Biblioteca de classe
+
+import javax.swing.*;
+
+public class constante {
+    public static void main (String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Valor de PI: " + Math.PI, "Constantes",1);
+    }
+}
